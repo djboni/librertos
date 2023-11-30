88d5f6056ad2d94d2448f0cb11e6926c36e3858f
Djones A. Boni <djboni@gmail.com>
2023-11-30 18:58:30 -0300

    Update build.zig (build CppUTest with zig build)

--- build.zig
+++ build.zig
@@ -1,10 +1,3 @@
-// git submodules update --init
-// mkdir -p tests/cpputest/cpputest_build; cd tests/cpputest/cpputest_build
-// CC="zig cc" CXX="zig c++" CMAKE_GENERATOR=Ninja cmake ..
-// cmake --build . --target src/CppUTest/libCppUTest.a --target src/CppUTestExt/libCppUTestExt.a
-// zig build
-// zig build test
-
 const std = @import("std");
 
 const cflags_c90 = [_][]const u8{
@@ -45,8 +38,69 @@ pub fn build(b: *std.Build) void {
     lib.addIncludePath(std.Build.LazyPath{ .path = "./include" });
     lib.addIncludePath(std.Build.LazyPath{ .path = "./examples/linux" });
     lib.addCSourceFiles(&[_][]const u8{
-        "src/librertos.c",
+        "./src/librertos.c",
     }, &cflags_c90);
+
+    const cpputest = b.addStaticLibrary(.{
+        .name = "CppUTest",
+        .root_source_file = null,
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(cpputest);
+    cpputest.linkLibC();
+    cpputest.linkLibCpp();
+    cpputest.addIncludePath(std.Build.LazyPath{ .path = "./tests/cpputest/include" });
+    cpputest.addCSourceFiles(&[_][]const u8{
+        "./tests/cpputest/src/CppUTest/CommandLineArguments.cpp",
+        "./tests/cpputest/src/CppUTest/MemoryLeakWarningPlugin.cpp",
+        "./tests/cpputest/src/CppUTest/TestHarness_c.cpp",
+        "./tests/cpputest/src/CppUTest/TestRegistry.cpp",
+        "./tests/cpputest/src/CppUTest/CommandLineTestRunner.cpp",
+        "./tests/cpputest/src/CppUTest/SimpleString.cpp",
+        "./tests/cpputest/src/CppUTest/SimpleStringInternalCache.cpp",
+        "./tests/cpputest/src/CppUTest/TestMemoryAllocator.cpp",
+        "./tests/cpputest/src/CppUTest/TestResult.cpp",
+        "./tests/cpputest/src/CppUTest/JUnitTestOutput.cpp",
+        "./tests/cpputest/src/CppUTest/TeamCityTestOutput.cpp",
+        "./tests/cpputest/src/CppUTest/TestFailure.cpp",
+        "./tests/cpputest/src/CppUTest/TestOutput.cpp",
+        "./tests/cpputest/src/CppUTest/MemoryLeakDetector.cpp",
+        "./tests/cpputest/src/CppUTest/TestFilter.cpp",
+        "./tests/cpputest/src/CppUTest/TestPlugin.cpp",
+        "./tests/cpputest/src/CppUTest/TestTestingFixture.cpp",
+        "./tests/cpputest/src/CppUTest/SimpleMutex.cpp",
+        "./tests/cpputest/src/CppUTest/Utest.cpp",
+        "./tests/cpputest/src/Platforms/Gcc/UtestPlatform.cpp",
+    }, &cflags_cpp11);
+
+    const cpputest_ext = b.addStaticLibrary(.{
+        .name = "CppUTestExt",
+        .root_source_file = null,
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(cpputest_ext);
+    cpputest_ext.linkLibC();
+    cpputest_ext.linkLibCpp();
+    cpputest_ext.addIncludePath(std.Build.LazyPath{ .path = "./tests/cpputest/include" });
+    cpputest_ext.addCSourceFiles(&[_][]const u8{
+        "./tests/cpputest/src/CppUTestExt/CodeMemoryReportFormatter.cpp",
+        "./tests/cpputest/src/CppUTestExt/GTest.cpp",
+        "./tests/cpputest/src/CppUTestExt/IEEE754ExceptionsPlugin.cpp",
+        "./tests/cpputest/src/CppUTestExt/MemoryReporterPlugin.cpp",
+        "./tests/cpputest/src/CppUTestExt/MockFailure.cpp",
+        "./tests/cpputest/src/CppUTestExt/MockSupportPlugin.cpp",
+        "./tests/cpputest/src/CppUTestExt/MockActualCall.cpp",
+        "./tests/cpputest/src/CppUTestExt/MockSupport_c.cpp",
+        "./tests/cpputest/src/CppUTestExt/MemoryReportAllocator.cpp",
+        "./tests/cpputest/src/CppUTestExt/MockExpectedCall.cpp",
+        "./tests/cpputest/src/CppUTestExt/MockNamedValue.cpp",
+        "./tests/cpputest/src/CppUTestExt/OrderedTest.cpp",
+        "./tests/cpputest/src/CppUTestExt/MemoryReportFormatter.cpp",
+        "./tests/cpputest/src/CppUTestExt/MockExpectedCallsList.cpp",
+        "./tests/cpputest/src/CppUTestExt/MockSupport.cpp",
+    }, &cflags_cpp11);
 
     const test_librertos = b.addExecutable(.{
         .name = "test_librertos",
@@ -62,34 +116,32 @@ pub fn build(b: *std.Build) void {
     test_librertos.addIncludePath(std.build.LazyPath{ .path = "./include" });
     test_librertos.addIncludePath(std.build.LazyPath{ .path = "./tests/port" });
     test_librertos.addCSourceFiles(&[_][]const u8{
-        "src/librertos.c",
+        "./src/librertos.c",
     }, &cflags_c90_testing);
 
     test_librertos.addIncludePath(std.Build.LazyPath{ .path = "./tests/cpputest/include" });
-    test_librertos.addLibraryPath(std.build.LazyPath{ .path = "./tests/cpputest/cpputest_build/src/CppUTest" });
-    test_librertos.addLibraryPath(std.build.LazyPath{ .path = "./tests/cpputest/cpputest_build/src/CppUTestExt" });
-    test_librertos.linkSystemLibrary("CppUTest");
-    test_librertos.linkSystemLibrary("CppUTestExt");
+    test_librertos.linkLibrary(cpputest);
+    test_librertos.linkLibrary(cpputest_ext);
 
     test_librertos.addIncludePath(std.build.LazyPath{ .path = "." });
     test_librertos.addCSourceFiles(&[_][]const u8{
         // Tests
-        "tests/librertos_delay_test.cpp",
-        "tests/librertos_event_test.cpp",
-        "tests/librertos_list_test.cpp",
-        "tests/librertos_sched_test.cpp",
-        "tests/librertos_tick_test.cpp",
-        "tests/librertos_timer_test.cpp",
-        "tests/concurrent_test.cpp",
+        "./tests/librertos_delay_test.cpp",
+        "./tests/librertos_event_test.cpp",
+        "./tests/librertos_list_test.cpp",
+        "./tests/librertos_sched_test.cpp",
+        "./tests/librertos_tick_test.cpp",
+        "./tests/librertos_timer_test.cpp",
+        "./tests/concurrent_test.cpp",
         // Test Events
-        "tests/semaphore_test.cpp",
-        "tests/mutex_test.cpp",
-        "tests/queue_test.cpp",
+        "./tests/semaphore_test.cpp",
+        "./tests/mutex_test.cpp",
+        "./tests/queue_test.cpp",
         // Supporting files
-        "tests/port/librertos_port.cpp",
-        "tests/mocks/librertos_assert.cpp",
-        "tests/utils/librertos_test_utils.cpp",
-        "tests/utils/librertos_custom_tests.cpp",
-        "tests/utils/main.cpp",
+        "./tests/port/librertos_port.cpp",
+        "./tests/mocks/librertos_assert.cpp",
+        "./tests/utils/librertos_test_utils.cpp",
+        "./tests/utils/librertos_custom_tests.cpp",
+        "./tests/utils/main.cpp",
     }, &cflags_cpp11);
 }
